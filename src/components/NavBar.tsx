d24eb7bb3032df59361d140b3ca29ecb2177ff2a
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2023-01-03 15:45:45 +0300

    created project / added navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
+import { TiMicrophone } from "react-icons/ti";
+import { BsYoutube, BsCameraVideo, BsBell } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoAppsSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
+const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center px-1 h-14 bg-[#212121] opacity-95 sticky top-0 z-50">
+      <div className="flex gap-8 items-center text-2xl">
+        <div>
+          <GiHamburgerMenu />
+        </div>
+        <Link to="/">
+          <div className="flex gap-1 items-center justify-center">
+            <BsYoutube className="text-3xl text-red-600" />
+            <span className="text-xl-font-medium">YouTube</span>
+          </div>
+        </Link>
+      </div>
+      <div className="flex items-center justify-center gap-5">
+        <form action="">
+          <div className="flex bg-zinc-900 items-center h-19 px-4 pr-0">
+            <div className="flex gap-4 items-center pr-5">
+              <div>
+                <AiOutlineSearch className="text-xl" />
+              </div>
+              <input
+                type="text"
+                className="w-96 bg-zinc-900 focus:outline-none border-none"
+              />
+              <AiOutlineClose className="h-10 w-16 flex items-center justify-center bg-zinc-800" />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
